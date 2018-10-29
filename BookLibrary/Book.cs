9753a199bb99ffb22472388352f8055f9b13f796
HannaZhuravskaya <hanna.zhuravskaya@gmail.com>
2018-10-29 11:46:47 +0300

    In process...

--- BookLibrary/Book.cs
+++ BookLibrary/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,29 @@ namespace BookLibrary
 {
     public class Book
     {
-        //TODO
+        public readonly string Title;
+
+        public readonly string Author;
+
+        public readonly int Year;
+
+        public readonly string PublishingHous;
+
+        public readonly int Edition;
+
+        public readonly int Pages;
+
+        public readonly double Price;
+
+        public Book(string title, string author, int year, string publishHous, int edition, int pages, double price)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            PublishingHous = publishHous;
+            Edition = edition;
+            Pages = pages;
+            Price = price;
+        }
     }
 }
